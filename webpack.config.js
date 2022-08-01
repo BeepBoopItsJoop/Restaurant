992bb37b7d8b0520edcca8e2acefc192a15935c9
Beepboopitsjoop <beepboopitsthejoop@gmail.com>
2022-08-01 17:27:15 +0300

    Added Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/JS/index.js',
@@ -6,6 +7,11 @@ module.exports = {
   devServer: {
     static: './dist',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
